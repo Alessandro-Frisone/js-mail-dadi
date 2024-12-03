f078d38457387af8c9ca01de6967c6852d1b2a3e
Alessandro-Frisone <alefri2004@gmail.com>
2024-12-03 16:02:48 +0100

    feat: finish second ex (dadi)

--- dadi/script.js
+++ dadi/script.js
@@ -1,13 +1,17 @@
 // Gioco dei dadi
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
-// Prima di partire a scrivere codice poniamoci qualche domanda:
-// Che ci sia un array da qualche parte?
-// Se dobbiamo confrontare qualcosa che "cosa" ci serve?
 
 // 1 - Generazione numero random da 1 a 6
 const punteggioGiocatore = Math.floor(Math.random() * 6);
 const punteggioComputer = Math.floor(Math.random() * 6);
-alert("GIOCATORE:" punteggioGiocatore)
-// 2 - Controllo del punteggio più alto
-if (punteggioGiocatore > punteggioComputer)
+// 2 - Stampa del punteggio generato
+alert("GIOCATORE: " + punteggioGiocatore + "\nCOMPUTER: " + punteggioComputer);
+// 3 - Controllo del punteggio più alto 
+if (punteggioGiocatore > punteggioComputer){
+    alert("Ha vinto il giocatore")
+}else if (punteggioGiocatore < punteggioComputer){
+    alert("Ha vinto il computer")
+}else {
+    alert("Pareggio!! Ritenta sarai più fortunato")
+}
